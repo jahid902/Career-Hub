df8e06c7594e79d0e411a1511ef0550194f6c1b9
jahid902 <jahidjr902@gmail.com>
2023-04-09 23:57:48 +0600

    completed header first section and made mobile responsive

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>hello from header.</h1>
+  return (
+    <div className="p-6">
+      <section className="flex flex-col-reverse md:grid grid-cols-2 gap-3 md:p-10">
+        <div className="font-serif space-y-6  md:order-first md:p-6">
+          <h1 className="font-bold text-4xl">
+            One step
+            <br />
+            Closer to your <br />
+            <span className="text-purple-800">Dream job.</span>
+          </h1>
+          <p>
+            Explore thousands of job opportunities with all the information you
+            need.<span className="font-semibold text-purple-700"> It's your future</span>. Come find it. Manage all your job application
+            from start to finish. This site will help you pursue your desired job and help you to 
+            reach your full potential.  
+          </p>
+          <button className="btn-common">Get started</button>
         </div>
-    );
+        <div className=" md:order-last">
+         <img className="w-full h-48 md:h-64 lg:h-96  rounded-lg object-contain" src="https://img.freepik.com/free-photo/bohemian-man-with-his-arms-crossed_1368-3542.jpg?size=626&ext=jpg&ga=GA1.1.1789606317.1681061526&semt=sph" alt="" />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Header;
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { BriefcaseIcon } from '@heroicons/react/24/solid'
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="flex justify-between sm:px-2 md:px-6 sm:pt-2 md:pt-4
-    bg-gradient-to-r from-purple-300 via-purple-200 to-purple-300 pb-2">
+    bg-gradient-to-r from-purple-300 via-fuchsia-300 to-purple-300 pb-2">
       <div className="flex items-center tracking-wider font-bold">
         <h1 className="text-3xl">
           Fit
@@ -15,14 +16,14 @@ const Navbar = () => {
       </div>
 
       <ul className="flex space-x-4 text-lg items-center">
-        <li>Home</li>
-        <li>Statistics</li>
-        <li>Applied jobs</li>
-        <li>Blogs</li>
+        <li><NavLink to="/" className={({isActive})=> isActive? 'active':'default'}>Home</NavLink></li>
+        <li><NavLink to="/statistics" className={({isActive})=> isActive? 'active':'default'}>Statistics</NavLink></li>
+        <li><NavLink to="/applications" className={({isActive})=> isActive? 'active':'default'}>Applied jobs</NavLink></li>
+        <li><NavLink to="/blogs" className={({isActive})=> isActive? 'active':'default'}>Blogs</NavLink></li>
       </ul>
 
       <div>
-        <button className="px-4 py-3 text-white bg-purple-700 rounded-lg hover:bg-purple-600">Start Applying</button>
+        <button className="btn-common">Start Applying</button>
       </div>
     </div>
   );
