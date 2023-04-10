14e40c98eef9cdd94686ea7fece627d17c9b6d82
jahid902 <jahidjr902@gmail.com>
2023-04-11 01:24:09 +0600

    created redirects file and added banner with image

--- src/component/Applications/Applications.jsx
+++ src/component/Applications/Applications.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const Applications = () => {
-    return (
-        <div>
-            <h1>hello from applications</h1>
-        </div>
-    );
+  return (
+    <div>
+      <div className="relative bg-slate-50 h-48 mb-4">
+        <h1 className="py-6 my-auto text-center text-3xl font-semibold text-fuchsia-800 mb-4">
+          Job Details
+        </h1>
+        <img
+          className="w-28 h-48 md:w-1/3 absolute inset-y-0 left-0 "
+          src="https://img.freepik.com/free-vector/blue-line-gradient-background_361591-1373.jpg?size=626&ext=jpg&uid=R98934714&ga=GA1.2.1789606317.1681061526&semt=sph"
+          alt=""
+        />
+        <img
+          className="w-28 h-48 md:w-1/3 absolute inset-y-0 right-0 "
+          src="https://img.freepik.com/free-vector/blue-line-gradient-background_361591-1373.jpg?size=626&ext=jpg&uid=R98934714&ga=GA1.2.1789606317.1681061526&semt=sph"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Applications;
--- src/component/JobDetails/JobDetails.jsx
+++ src/component/JobDetails/JobDetails.jsx
@@ -30,9 +30,13 @@ const JobDetails = () => {
 
   return (
     <div className="w-11/12 bg-slate-100 mx-auto font-serif p-5 md:p-10">
-      <h1 className="py-6 text-center text-3xl font-semibold text-fuchsia-800 mb-4">
+      <div className="relative bg-slate-50 h-48 mb-4">
+      <h1 className="py-6 my-auto text-center text-3xl font-semibold text-fuchsia-800 mb-4">
         Job Details
       </h1>
+      <img className="w-20 h-48 md:w-72 absolute inset-y-0 left-0 " src="https://img.freepik.com/free-vector/blue-line-gradient-background_361591-1373.jpg?size=626&ext=jpg&uid=R98934714&ga=GA1.2.1789606317.1681061526&semt=sph" alt="" />
+      <img className="w-20 h-48 md:w-72 absolute inset-y-0 right-0 " src="https://img.freepik.com/free-vector/blue-line-gradient-background_361591-1373.jpg?size=626&ext=jpg&uid=R98934714&ga=GA1.2.1789606317.1681061526&semt=sph" alt="" />
+      </div>
       <div className="flex flex-col md:flex-row gap-5 justify-between">
         <div className="space-y-4 md:space-y-8 w-full">
           <p>
