ebcb05a8fdccab06a441d4925c4cea2b9a5718b6
jahid902 <jahidjr902@gmail.com>
2023-04-11 19:08:58 +0600

    upgraded statistics chart

--- src/component/Statistics/Statistics.jsx
+++ src/component/Statistics/Statistics.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
   const marks = [
@@ -7,43 +7,43 @@ const Statistics = () => {
       id : 1,  
       name: "Assignment-1",
       full_mark: 60,
-      achieved: 57,
+      achieved: 10,
     },
     {
       id : 2,  
       name: "Assignment-2",
       full_mark: 60,
-      achieved: 59,
+      achieved: 55,
     },
     {
       id : 3,  
       name: "Assignment-3",
       full_mark: 60,
-      achieved: 59,
+      achieved: 25,
     },
     {
       id : 4,  
       name: "Assignment-4",
       full_mark: 60,
-      achieved: 60,
+      achieved: 10,
     },
     {
       id : 5,  
       name: "Assignment-5",
       full_mark: 60,
-      achieved: 60,
+      achieved: 20,
     },
     {
       id : 6,  
       name: "Assignment-6",
       full_mark: 60,
-      achieved: 58,
+      achieved: 38,
     },
     {
       id : 7,  
       name: "Assignment-7",
       full_mark: 60,
-      achieved: 60,
+      achieved: 40,
     },   
     {
       id : 8,  
@@ -56,19 +56,18 @@ const Statistics = () => {
   return <div className="bg-slate-200 py-3 text-fuchsia-700 ">
         <h1 className="py-10 text-3xl font-bold text-center tracking-wide">Assignment Marks Statistics</h1>
         <ResponsiveContainer width="95%" height={400}>
-        <BarChart
-          width={1000}
+        <AreaChart
+          width={500}
           height={400}
-          data={marks}        
+          data={marks}
+          
         >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="full_mark" fill="#8884d8" />
-        <Bar dataKey="achieved" fill="#82ca9d" />
-        </BarChart>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="achieved" stackId="1" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
         </ResponsiveContainer>
         </div>;
 };
